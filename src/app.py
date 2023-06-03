0dc2f38d567931e446dc3a60e4bceb0997999dd1
Alberto Nieto <94092485+KingMuerte@users.noreply.github.com>
2023-06-03 13:55:19 +0000

    Move app.py Add Input & Save Periods

---
+++ src/app.py
@@ -0,0 +1,34 @@
+import calendar
+from datetime import datetime
+
+import debugpy
+import plotly.graph_objects as go
+import streamlit as st
+
+debugpy.listen(("localhost", 5678))
+
+
+# ---------SETTINGS--------------
+income = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = ["USD"]
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"
+layout = "centered"
+# -------------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+# ---------------- DROP DOWN VALUES FOR SELECTING THE PERIOD ---------------
+
+years = [datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
+
+
+# -------------- INPUT & SAVE PERIODS -----------------------------
+st.header(f"Data Entry in {currency}")
+with st.form("engry_form", clear_on_submit=True):
+    col1, col2 = st.columns(2)
+    col1.selectbox("Select Month:", months, key="month")
+    col2.selectbox("Select Year:", years, key="year")
