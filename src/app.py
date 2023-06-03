57fa3a520513c0da9ee11d6b474ac05d6f07eaf9
Alberto Nieto <94092485+KingMuerte@users.noreply.github.com>
2023-06-03 15:30:42 +0000

    Add input sections income expense comment Add submit button write out values show success status

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ import plotly.graph_objects as go
 import streamlit as st
 
 # ---------SETTINGS--------------
-income = ["Salary", "Blog", "Other Income"]
+incomes = ["Salary", "Blog", "Other Income"]
 expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
-currency = ["USD"]
+currency = "USD"
 page_title = "Income and Expense Tracker"
 page_icon = ":money_with_wings:"
 layout = "centered"
@@ -28,3 +28,24 @@ with st.form("engry_form", clear_on_submit=True):
     col1, col2 = st.columns(2)
     col1.selectbox("Select Month:", months, key="month")
     col2.selectbox("Select Year:", years, key="year")
+
+    "---"
+    with st.expander("Income"):
+        for income in incomes:
+            st.number_input(f"{income}:", min_value=0,format="%i", step=10, key=income)
+    with st.expander("Expenses"):
+        for expense in expenses:
+            st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
+    with st.expander("Comment"):
+        comment = st.text_area("", placeholder="Enter a comment here ...")
+
+    "---"
+    submitted = st.form_submit_button("Save Data")
+    if submitted:
+        period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
+        incomes = {income: st.session_state[income] for income in incomes}
+        expenses = {expense: st.session_state[expense] for expense in expenses}
+        # TODO Insert values into database
+        st.write(f"incomes: {incomes}")
+        st.write(f"expenses: {expenses}")
+        st.success("Data Saved!")
